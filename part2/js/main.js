0544a93c9bb76193fd8c79964e5ddb1db92a924a
johnny1864 <johnny1864@yahoo.com>
2018-05-10 19:19:33 -0700

    finished slider

--- part2/js/main.js
+++ part2/js/main.js
@@ -1,3 +1,5 @@
+/*eslint-env browser*/
+
 $(document).ready(function(){
     //MOVE TO THE NEXT IMAGE
    $("#right-arrow").click(function(){
@@ -6,11 +8,11 @@ $(document).ready(function(){
        
        if(nextImg.length){
            currentImg.removeClass('active').css('z-index', -10);
-           nextImg.addClass('active').css('z-index', 10);
+           nextImg.addClass('active');
        }else{
            currentImg.removeClass('active').css('z-index', -10);
            $('.slider-img img').first().addClass('active').css('z-index', 10);
-           $('ul li').first().addClass('active').css('z-index', 10);
+           $('ul li').first().addClass('active');
        }
    });
     
@@ -21,19 +23,26 @@ $(document).ready(function(){
        
        if(prevImg.length){
            currentImg.removeClass('active').css('z-index', -10);
-           prevImg.addClass('active').css('z-index', 10);
+           prevImg.addClass('active');
        }else{
            currentImg.removeClass('active').css('z-index', -10);
            $('.slider-img img').last().addClass('active').css('z-index', 10);
-           $('ul li').last().addClass('active').css('z-index', 10);
+           $('ul li').last().addClass('active');
        }
    });
     
     
     //DISPLAY IMAGES SELECTED 
-    
-    $('ul').click(function(e){
-        console.log(e.target);
+    var currentImg = 1;
+    $('li').click(function(e){
+        //console.log(this);
+        $('li').removeClass('active');
+        $(this).addClass('active');
+        
+        
+        currentImg = $(this).index()+1;
+        $('.slider-img img').removeClass('active').fadeOut();
+        $('.slider-img img:nth-of-type('+currentImg+')').addClass('active');
     });
 });
 
