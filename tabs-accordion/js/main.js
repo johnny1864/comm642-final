a4c4c5211feb74274952874e817a9f48c97073f4
Johnny <johnny1864@yahoo.com>
2018-05-17 18:17:22 -0700

    added images

--- tabs-accordion/js/main.js
+++ tabs-accordion/js/main.js
@@ -1,39 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function () {
     'use strict';
-    
-    function tabs(){
+
+    function tabs() {
         $('#tab-content div').hide();
         $('#tab1').show();
-        $('a').click(function(e){
-            //console.log(e.target);
-            $('#tabs li').removeClass('active');
-            if(e.target.id === 'to-tab1'){
-                $('#tab-content div').hide();
-                $('#tab1').show();
-                $(this).parent().addClass('active');
-            }else if(e.target.id === 'to-tab2'){
-                $('#tab-content div').hide();
-                $('#tab2').show();
-                $(this).parent().addClass('active');
-            }else if(e.target.id === 'to-tab3'){
-                $('#tab-content div').hide();
-                $('#tab3').show();
-                $(this).parent().addClass('active');
-            }
-            
-            
-        });
+        $('a').click(displayTab);
+        $('a').mouseenter().mouseleave();
     }
+
+    function displayTab(e) {
+        $('#tabs li').removeClass('active');
+        if (e.target.id === 'to-tab1') {
+            $('#tab-content div').hide();
+            $('#tab1').show();
+            $(this).parent().addClass('active');
+        } else if (e.target.id === 'to-tab2') {
+            $('#tab-content div').hide();
+            $('#tab2').show();
+            $(this).parent().addClass('active');
+        } else if (e.target.id === 'to-tab3') {
+            $('#tab-content div').hide();
+            $('#tab3').show();
+            $(this).parent().addClass('active');
+        }
+    }
+    
     tabs();
-    
-    
-    function accordion(){
-        $('button').click(function(e){
-            var parent = $(this).parent()[0].id;
-            $(this).toggleClass('clicked'); 
-            $(`#${parent} p `).toggleClass('block');   
-        })
-    }
-    
-    accordion();
+
+
+function accordion() {
+    $('button span').addClass('plus');
+    $('button').click(function (e) {
+        var parent = $(this).parent()[0].id;
+        $(this).toggleClass('clicked');
+        $(`#${parent} p `).toggleClass('block');
+        
+         $('button span').;
+        //console.log(plusSign);
+        
+    });
+}
+
+accordion();
 });
